3a6522a778f653e8691f77b78ebe49c7c124f061
Jeishy <k1519236@kingston.ac.uk>
2019-05-03 06:09:01 +0100

    Started adding sound

---
+++ Omicron/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    private void Awake() 
+    {
+        foreach (Sound s in sounds)
+        {
+            // Set clip, volume and pitch variables the sound's clip, volume and pitch
+            s.Source = gameObject.AddComponent<AudioSource>();
+            s.Source.clip = s.Clip;
+            s.Source.volume = s.Volume;
+            s.Source.pitch = s.Pitch;
+        }
+    }
+
+    private void Start() {
+        Play("Test");
+    }
+
+    public void Play (string name)
+    {
+        // Find sound in the sounds array
+        Sound s = Array.Find(sounds, sound => sound.Name == name);
+        // Do null check and throw error if sound of specified name does not exist
+        if (s == null)
+        {
+            Debug.LogError("Sound: " + name + " not found!");
+            return;
+        }
+        // Play the sound if sound is found
+        s.Source.Play();
+    }
+}
---
+++ Omicron/Assets/Scripts/Audio/Sound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound 
+{
+    // Name of the sound
+    public string Name;
+    // Audio clip of the sound
+    public AudioClip Clip;
+    // The sound's volume
+    [Range(0f, 1f)]
+    public float Volume;
+    // The sound's pitch
+    [Range(0.1f, 3f)]
+    public float Pitch;
+    // Audio source that sound plays from
+    [HideInInspector] public AudioSource Source;
+}
